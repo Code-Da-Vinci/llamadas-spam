1ed2858bdc5a8aea3a27780c20c4dffb39e2467f
Daniel Rojas <erojashi@isise.edu.pe>
2024-06-25 00:59:24 -0500

    Agregando formulario, eliminando plugin facebook y mejorando funcion buscar

--- main.js
+++ main.js
@@ -1,29 +1,73 @@
 const entrada = document.querySelector(".header__input");
 const gif = document.querySelector(".header__gif");
 const mensaje = document.querySelector(".header__mensaje");
+const seccionSpam = document.querySelector(".section1__spam");
+
 const compartir = document.querySelector(".footer__icon-mod");
 const span = document.querySelector(".footer__span");
 const spanSms = document.querySelector(".footer__copiado");
 
-let arreglo = ["972229776", "94651944", "956456207", "930143374", "962758201", "952414598", "924489743","942290145","964308333","973511215","948769612"];
-
 let url = "https://llamadas-spam-peru.netlify.app";
 
+compartir.addEventListener("click", () => {
+   navigator.clipboard.writeText(url);
+   span.style.display = "flex";
+   spanSms.textContent = "Url copiado para compartir";
+
+   setTimeout(() => {
+      span.style.display = "none";
+      spanSms.textContent = "";
+      
+   }, 5000);
+});
+
+let arreglo = ["972229776", "94651944", "956456207", "930143374", "962758201", "952414598", "924489743","942290145","964308333","973511215"];
+
+//----- Agregando números spam al DOM ----
+
+arreglo.forEach(spam => {
+   seccionSpam.insertAdjacentHTML('beforeend', `<span class="section1__nro">${spam}</span>`);
+
+});
+
+//----- Buscador -----
+
+const nrosVisibles = document.querySelectorAll(".section1__nro");
+
 function buscar(ev) {
-   let ingreso = entrada.value;
-   
+   let ingreso = entrada.value.trim();
+   let ubicado = false; // Variable para rastrear si se encuentra una coincidencia
+
    if (ingreso === "") {
       alert("Ingresa un número");
 
    } else {
-      if (arreglo.includes(ingreso)) {
+      nrosVisibles.forEach((valor) => {
+         if (valor.textContent.trim() === ingreso) {
+            valor.style.display = "flex"; // Mostrar el número que coincide
+            valor.style.backgroundColor = "var(--color-2)";
+            valor.style.color = "var(--color-1)";
+
+            ubicado = true; // Se encontró una coincidencia
+
+         } else {
+            valor.style.display = "none"; // Ocultar los demás números
+            valor.style.backgroundColor = "var(--color-1)";
+            valor.style.color = "var(--color-2)";
+
+         };
+      });
+
+      if (ubicado) {
          mensaje.textContent = `☠️ Número reportado como Spam`;
-         mensaje.style.color = `var(--color-3)`;
-   
+
       } else {
          mensaje.textContent = "No se encontraron coincidencias";
-         mensaje.style.color = `var(--color-3)`;
-   
+         // Mostrar todos los números si no hay coincidencias
+
+         nrosVisibles.forEach((valor) => {
+            valor.style.display = "flex";
+         });
       };
    };
 };
@@ -36,7 +80,6 @@ entrada.addEventListener("keydown", (ev) => {
 
    if (ev.code === "Backspace") {
       mensaje.textContent = "...";
-      mensaje.style.color = `var(--color-2)`;
 
    };
 });
@@ -48,14 +91,45 @@ gif.addEventListener("click", (ev) => {
    
 buscar;
 
-compartir.addEventListener('click', () => {
-   navigator.clipboard.writeText(url);
-   //console.log(navigator.clipboard = url);
-   span.style.display = "flex";
-   spanSms.innerHTML = "¡Link copiado!";
-   
-   setTimeout(() => {
-      span.style.display = "none";
 
-   }, 4000);
+//-----  formulario -----
+
+const formulario = document.querySelector(".form");
+const inputs = document.querySelectorAll(".form [required]");
+const enviar = document.querySelector(".form__enviar");
+
+inputs.forEach(input =>  {
+   const crearSpan = document.createElement("span");
+   crearSpan.id = input.name;
+   crearSpan.textContent = input.title;
+   crearSpan.classList.add("form__error", "none");
+   input.insertAdjacentElement('afterend', crearSpan);
+  
+});
+
+document.addEventListener("keyup", (e) => {
+   if (e.target.matches(".form [required]")) {
+      let valorInput = e.target,
+      patern = valorInput.pattern || valorInput.dataset.pattern;
+      console.log(valorInput, patern);
+
+      if (patern && valorInput.value !== "") {
+         let regex = new RegExp(patern);
+
+         return !regex.exec(valorInput.value)
+         ? document.getElementById(valorInput.name).classList.add("active")
+         : document.getElementById(valorInput.name).classList.remove("active");
+      };
+
+      if (!patern) {
+         return valorInput.value === ""
+         ? document.getElementById(valorInput.name).classList.add("active")
+         : document.getElementById(valorInput.name).classList.remove("active");
+      };
+   };
+});
+
+enviar.addEventListener("submit", (e) => {
+   formulario.reset();
+
 });
